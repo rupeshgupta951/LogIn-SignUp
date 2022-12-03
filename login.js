40d220dff0ab583e521104f3743431dfd2644198
Rupesh Gupta <rupeshid951@gmail.com>
2022-12-03 09:53:59 +0530

    initial commit

---
+++ login.js
@@ -0,0 +1,12 @@
+ let node = document.querySelector('.container');
+ node = node.children;
+ console.log(node);
+//  let nodeName = node.childNodes[1].nodeName;
+//  console.log(nodeName);
+
+// let cont = document.querySelector('.no');
+// cont = document.querySelector('.container');
+// let nodeName = cont.childNodes[1].nodeName;
+// let nodeType = cont.childNodes[1].nodeType;
+//  console.log(nodeName)
+//  console.log(nodeType)
